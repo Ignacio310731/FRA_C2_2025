6cad5ded4fd9e04cfe185f1879f8703235cab83e
Ignacio310731 <nacho2007clubtucuman@gmail.com>
2025-10-02 19:28:28 -0300

    Update Ignacio_Andrada.py

--- semana 4/clase 8/Ignacio_Andrada.py
+++ semana 4/clase 8/Ignacio_Andrada.py
@@ -1,26 +1,9 @@
-lista = [7, 3, 5, 1]
-n = len(lista)
-for i in range(n):
-    for j in range(0, n-1-i):
-        if lista[j] < lista[j+1]:
-            lista[j], lista[j+1] = lista[j+1], lista[j]
-print(lista)
-
-
-
-
-
-
-
-
-
-
 MAX = 10
 lista_nombres = [""] * MAX
 lista_puntajes = [0] * MAX
 lista_comentarios = [""] * MAX
 
-# ---------- Función de validación de ingreso de textos -----------
+
 def ingresar_texto(mensaje):
     texto = ""
     while texto == "":
@@ -30,7 +13,7 @@ def ingresar_texto(mensaje):
     
     return texto
 
-# ------------ Función para validar puntajes de 1 a 10 ----------
+
 def ingresar_puntaje(mensaje):
     while True:
         numero = int(input(mensaje))
@@ -39,7 +22,6 @@ def ingresar_puntaje(mensaje):
         else:
             print("Debe ingresar in número entre 1 y 10")
 
-# --------- Carga de datos --------------------
 def ingresar_datos(lista_nombres, lista_puntajes, lista_comentarios, cantidad):
     while cantidad < MAX:
         print(f"------ Participante {cantidad + 1} ------")
@@ -52,7 +34,6 @@ def ingresar_datos(lista_nombres, lista_puntajes, lista_comentarios, cantidad):
             break
     return cantidad
 
-# ------------ Moostrar datos ---------------
 def mostrar_datos(nombres, puntajes, comentarios, cantidad):
     if cantidad == 0:
         print("No hay datos cargados...")
@@ -65,7 +46,6 @@ def mostrar_datos(nombres, puntajes, comentarios, cantidad):
     promedio = suma / cantidad
     print(f"Promedio de puntaje total: {promedio:.2f}")
 
-# ---------- Ordenar listas ----------------------
 def ordenar_por_puntaje(nombres, puntajes, comentarios, cantidad):
     for i in range(cantidad - 1):
         for j in range(cantidad - 1 - i):
@@ -78,7 +58,6 @@ def ordenar_por_puntaje(nombres, puntajes, comentarios, cantidad):
     for i in range(cantidad):
         print(f"{i + 1}. {nombres[i]} - Puntaje: {puntajes[i]} - Comentario: {comentarios[i]}")
 
-# --------- Programa principal ------------------
 cantidad = 0
 
 while True:
@@ -97,7 +76,7 @@ while True:
     elif opcion == "3":
         ordenar_por_puntaje(lista_nombres, lista_puntajes, lista_comentarios, cantidad)
     elif opcion == "4":
-        print("Gracias por participar en la encuesta :)")
+        print("Gracias por participar en la encuesta )")
         break
     else:
         print("Opción inválida, intente nuevamente.")
